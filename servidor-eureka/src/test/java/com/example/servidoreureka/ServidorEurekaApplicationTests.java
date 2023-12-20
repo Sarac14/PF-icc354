ab4dbb7d0d7b5bf5f8193aef8c7ff2ffb444b7da
Sara Contreras <smct0001@ce.pucmm.edu.do>
2023-12-20 01:20:06 -0400

    servidores x2

---
+++ servidor-eureka/src/test/java/com/example/servidoreureka/ServidorEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servidoreureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
